48f57f4ef63cfed817282abc6764c25827c96d1c
Estevao Castro <78557654+EstevaoCa@users.noreply.github.com>
2024-01-17 17:32:22 +0000

    Adicionando interacoes nos input ao conteudo

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./Imagens/aprovado.png" alt="Emoji celebrando"/>';
+const imgReprovado = '<img src="./Imagens/reprovado.png" alt="Emoji decepcionado"/>';
+
+
+linhas = '';
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+
+    let linha = '<tr>';
+    linha += `<td> ${inputNomeAtividade.value}</td>`;
+    linha += `<td> ${inputNotaAtividade.value}</td>`;
+    linha += `<td> ${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado } </td>`;
+    linha += '</tr>'
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+})
