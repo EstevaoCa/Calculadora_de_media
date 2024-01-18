ef9fc81c7fd501a6e1266b8df7cf07f4091844f1
Estevao Castro <78557654+EstevaoCa@users.noreply.github.com>
2024-01-18 21:11:01 +0000

    limitação de casas decimais

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function atualizarTabela(){
 
 function atualizarMediaFinal(){
     const mediaFinal = calculaMediaFinal();
-    document.getElementById('media_final_valor').innerHTML = mediaFinal;
+    document.getElementById('media_final_valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media_final_resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 }
